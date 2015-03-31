a789ab67e93abe8352d3bc7820eae54205b4c223
greg <greg@softsprocket.com>
2015-03-31 13:43:25 -0700

    fixed document init

--- modules/datam/document.js
+++ modules/datam/document.js
@@ -6,10 +6,42 @@ function Document (obj) {
 	}
 }
 
+function Document.Clone (obj) {
+	var copy;
+
+	if (obj instanceof Date) {
+		copy = new Date ();
+		copy.setTime (obj.getTime ());
+		return copy;
+	}
+
+	if (obj instanceof Array) {
+		copy = [];
+		for (var i = 0, len = obj.length; i < len; i++) {
+			copy[i] = Document.Clone (obj[i]);
+		}
+		return copy;
+	}
+
+	if (obj instanceof Object) {
+		copy = {};
+		for (var attr in obj) {
+			if (obj.hasOwnProperty (attr)) copy[attr] = Document.Clone (obj[attr]);
+		}
+		return copy;
+	}
+
+	throw Error ('unsupported object type');
+}
+
 Document.prototype.init = function (data) {
 	for (each in data) {
 		if (data.hasOwnProperty (each)) {
-			this[each] = data[each];
+			if (data[each] typeof 'object') {
+				this[each] = Document.Clone (data[each]);
+			} else {
+				this[each] = data[each];
+			}
 		}
 	}
 }
@@ -19,7 +51,11 @@ Document.prototype.data = function () {
 
 	for (each in this) {
 		if (this.hasOwnProperty (each) && typeof this[each] != 'function') {
-			data[each] = this[each];
+			if (data[each] typeof 'object') {
+				data[each] = Document.Clone (this[each]);
+			} else {
+				data[each] = this[each];
+			}
 		}
 	}
 
